11744cc53807b6f75272bde94ce2694ccb0a94a1
Krishna <krishnaarana338@gmail.com>
2024-08-06 00:42:11 +0530

    Added 2-2.asm

---
+++ 2-2.asm
@@ -0,0 +1,21 @@
+// ADDITION OF TWO 8 BIT NUMBERS
+// Manually strore 1st no in the memory location C050
+// Manually store 2nd no in the memory location C051
+// Result is stored in C053
+# BEGIN 0000H
+	   MVI A, 55H
+	   MVI B, 76H
+	CMP B
+	   JC LABEL
+	   SUB B
+	   STA C052
+	   HLT
+
+LABEL:
+	SUB B
+	CMA
+	INR A
+	STA C052h
+	HLT
+// EXAMPLE-> C050 = 55H, C051 =66H
+// ANSWER -> C052 = BBH
